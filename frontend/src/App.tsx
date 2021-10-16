f2d897cbf132713831a5f289b582984ef2787251
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2021-10-16 09:12:33 -0700

    Add home button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import * as axios from "axios";
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
 import logo from './logo.svg';
 import './App.css';
 import "./bootstrap.min.css";
@@ -16,9 +16,9 @@ const httpClient = axios.default;
 const client = new AIBrushApi(undefined, config.apiUrl, httpClient);
 
 function updateHttpClient(loginResult: LoginResult) {
-    if (loginResult.accessToken) {
-        httpClient.defaults.headers.common['Authorization'] = `Bearer ${loginResult.accessToken}`;
-    }
+  if (loginResult.accessToken) {
+    httpClient.defaults.headers.common['Authorization'] = `Bearer ${loginResult.accessToken}`;
+  }
 }
 
 function App() {
@@ -61,11 +61,19 @@ function App() {
         {/* if credentials are not set, show Login component */}
         {!credentials && <Login httpClient={httpClient} client={client} onLogin={onLogin} />}
         {/* if credentials are set, show a bootstrap logout button a the far top right corner div */}
-        {credentials && <button className="btn btn-primary logout-button" onClick={() => setCredentials(null)}>
-          {/* font awesome logout icon */}
-          <i className="fas fa-sign-out-alt"></i>&nbsp;
-          Logout
-        </button>}
+        {credentials && <>
+          <button className="btn btn-primary top-button" onClick={() => setCredentials(null)}>
+            {/* font awesome logout icon */}
+            <i className="fas fa-sign-out-alt"></i>&nbsp;
+            Logout
+          </button>
+          {/* home button */}
+          <Link className="btn btn-primary top-button" to="/">
+            {/* font awesome home icon */}
+            <i className="fas fa-home"></i>&nbsp;
+            Home
+          </Link>
+        </>}
         {/* if credentials are set, show the rest of the app */}
         {credentials && <Switch>
           <Route path="/" exact={true}>
