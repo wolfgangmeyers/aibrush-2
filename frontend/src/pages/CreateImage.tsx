073d16a4587b73a79747d58da478202fb625e1f5
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-01-07 18:34:44 -0800

    Re-enable zoom in the UI

--- frontend/src/pages/CreateImage.tsx
+++ frontend/src/pages/CreateImage.tsx
@@ -182,130 +182,130 @@ export const CreateImage: FC<CreateImageProps> = (props) => {
 
     return (
         <>
-                {/* Header: Create new image */}
-                <div className="row">
-                    <div className="col-12">
-                        <h1>Create new image</h1>
-                    </div>
+            {/* Header: Create new image */}
+            <div className="row">
+                <div className="col-12">
+                    <h1>Create new image</h1>
                 </div>
-                {/* 50px vertical spacer */}
-                <div className="row">
-                    <div className="col-12">
-                        <div className="spacer" />
-                    </div>
+            </div>
+            {/* 50px vertical spacer */}
+            <div className="row">
+                <div className="col-12">
+                    <div className="spacer" />
                 </div>
-                <div className="row">
-                    <div className="offset-lg-3 col-lg-6 col-sm-12">
-                        <form onSubmit={onSubmit}>
-                            <div className="form-group">
-                                <label>Phrases</label>
-                                <input
-                                    className="form-control"
-                                    type="text"
-                                    value={input.phrases?.join("|")}
-                                    onChange={(e) => setInput({ ...input, phrases: e.target.value.split("|") })}
-                                    placeholder="Separate | phrases | like this" />
+            </div>
+            <div className="row">
+                <div className="offset-lg-3 col-lg-6 col-sm-12">
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group">
+                            <label>Phrases</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                value={input.phrases?.join("|")}
+                                onChange={(e) => setInput({ ...input, phrases: e.target.value.split("|") })}
+                                placeholder="Separate | phrases | like this" />
+                        </div>
+                        <div className="form-group">
+                            <label>Label</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                value={input.label}
+                                onChange={(e) => setInput({ ...input, label: e.target.value })}
+                                placeholder="Label" />
+                        </div>
+                        <div className="form-group">
+                            <label>Iterations</label>
+                            <input min={1} max={10000} className="form-control" type="number" value={input.iterations} onChange={(e) => setInput({ ...input, iterations: parseInt(e.target.value) })} />
+                        </div>
+                        {/* count */}
+                        <div className="form-group">
+                            <label>Count</label>
+                            <input className="form-control" type="number" max={10} min={1} value={count} onChange={(e) => setCount(parseInt(e.target.value))} />
+                        </div>
+                        {/* boolean enable_video (bootstrap styled checkbox) */}
+                        <div className="form-group">
+                            <label style={{ marginRight: "10px" }}>Enable video</label>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" checked={input.enable_video} onChange={(e) => setInput({ ...input, enable_video: e.target.checked })} />
                             </div>
-                            <div className="form-group">
-                                <label>Label</label>
-                                <input
-                                    className="form-control"
-                                    type="text"
-                                    value={input.label}
-                                    onChange={(e) => setInput({ ...input, label: e.target.value })}
-                                    placeholder="Label" />
-                            </div>
-                            <div className="form-group">
-                                <label>Iterations</label>
-                                <input min={1} max={10000} className="form-control" type="number" value={input.iterations} onChange={(e) => setInput({ ...input, iterations: parseInt(e.target.value) })} />
-                            </div>
-                            {/* count */}
-                            <div className="form-group">
-                                <label>Count</label>
-                                <input className="form-control" type="number" max={10} min={1} value={count} onChange={(e) => setCount(parseInt(e.target.value))} />
-                            </div>
-                            {/* boolean enable_video (bootstrap styled checkbox) */}
-                            <div className="form-group">
-                                <label style={{ marginRight: "10px" }}>Enable video</label>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" checked={input.enable_video} onChange={(e) => setInput({ ...input, enable_video: e.target.checked })} />
-                                </div>
-                            </div>
-                            {/* boolean enable_zoom (bootstrap styled checkbox) */}
-                            {/* {input.enable_video && <div className="form-group">
-                            <label style={{marginRight: "10px"}}>Enable zoom</label>
+                        </div>
+                        {/* boolean enable_zoom (bootstrap styled checkbox) */}
+                        {input.enable_video && <div className="form-group">
+                            <label style={{ marginRight: "10px" }}>Enable zoom</label>
                             <div className="form-check">
                                 <input className="form-check-input" type="checkbox" checked={input.enable_zoom} onChange={(e) => setInput({ ...input, enable_zoom: e.target.checked })} />
                             </div>
-                        </div>} */}
+                        </div>}
 
-                            {/* if enable_zoom, show zoom_interval input */}
-                            {input.enable_zoom && <div className="form-group">
-                                <label>Zoom interval</label>
-                                <input min={1} max={100} className="form-control" type="number" value={input.zoom_frequency} onChange={(e) => setInput({ ...input, zoom_frequency: parseInt(e.target.value) })} />
-                            </div>}
-                            {/* if enable_zoom, show zoom_scale input */}
-                            {input.enable_zoom && <div className="form-group">
-                                <label>Zoom scale</label>
-                                <input min={0.1} max={10} step={0.01} className="form-control" type="number" value={input.zoom_scale} onChange={(e) => setInput({ ...input, zoom_scale: parseFloat(e.target.value) })} />
-                            </div>}
-                            {/* if enable_zoom, show zoom_shift_x input */}
-                            {input.enable_zoom && <div className="form-group">
-                                <label>Zoom shift x</label>
-                                <input min={-10} max={10} className="form-control" type="number" value={input.zoom_shift_x} onChange={(e) => setInput({ ...input, zoom_shift_x: parseInt(e.target.value) })} />
-                            </div>}
-                            {/* if enable_zoom, show zoom_shift_y input */}
-                            {input.enable_zoom && <div className="form-group">
-                                <label>Zoom shift y</label>
-                                <input min={-10} max={10} className="form-control" type="number" value={input.zoom_shift_y} onChange={(e) => setInput({ ...input, zoom_shift_y: parseInt(e.target.value) })} />
-                            </div>}
+                        {/* if enable_zoom, show zoom_interval input */}
+                        {input.enable_zoom && <div className="form-group">
+                            <label>Zoom interval</label>
+                            <input min={1} max={100} className="form-control" type="number" value={input.zoom_frequency} onChange={(e) => setInput({ ...input, zoom_frequency: parseInt(e.target.value) })} />
+                        </div>}
+                        {/* if enable_zoom, show zoom_scale input */}
+                        {input.enable_zoom && <div className="form-group">
+                            <label>Zoom scale</label>
+                            <input min={0.1} max={10} step={0.01} className="form-control" type="number" value={input.zoom_scale} onChange={(e) => setInput({ ...input, zoom_scale: parseFloat(e.target.value) })} />
+                        </div>}
+                        {/* if enable_zoom, show zoom_shift_x input */}
+                        {input.enable_zoom && <div className="form-group">
+                            <label>Zoom shift x</label>
+                            <input min={-10} max={10} className="form-control" type="number" value={input.zoom_shift_x} onChange={(e) => setInput({ ...input, zoom_shift_x: parseInt(e.target.value) })} />
+                        </div>}
+                        {/* if enable_zoom, show zoom_shift_y input */}
+                        {input.enable_zoom && <div className="form-group">
+                            <label>Zoom shift y</label>
+                            <input min={-10} max={10} className="form-control" type="number" value={input.zoom_shift_y} onChange={(e) => setInput({ ...input, zoom_shift_y: parseInt(e.target.value) })} />
+                        </div>}
 
-                            {/* If encoded_image (base64 only) is set, show the image using a base64 image url*/}
-                            {input.encoded_image && <div className="form-group">
-                                <h5>Initial Image</h5>
-                                <img alt="" src={`data:image/jpeg;base64,${input.encoded_image}`} style={{ maxWidth: "100%" }} />
-                            </div>}
-                            {/* If encoded_image is set, display edit button */}
-                            <div className="form-group">
-                                <label
-                                    id="loadimage-wrapper"
-                                    className={`btn btn-sm btn-primary btn-file`}
-                                    style={{ marginTop: "8px", marginRight: "8px" }}
-                                >
-                                    {input.encoded_image ? "Replace Image" : "Upload Image"}
-                                    <input
-                                        id="loadimage"
-                                        type="file"
-                                        style={{ display: "none" }}
-                                        onChange={e => onImageSelected(e)}
-                                    />
-                                </label>
-                                <button type="button" className="btn btn-sm btn-primary" onClick={onEditImage}>Edit Image</button>
-                                
-                            </div>
-                            {/* <div className="form-group">
+                        {/* If encoded_image (base64 only) is set, show the image using a base64 image url*/}
+                        {input.encoded_image && <div className="form-group">
+                            <h5>Initial Image</h5>
+                            <img alt="" src={`data:image/jpeg;base64,${input.encoded_image}`} style={{ maxWidth: "100%" }} />
+                        </div>}
+                        {/* If encoded_image is set, display edit button */}
+                        <div className="form-group">
+                            <label
+                                id="loadimage-wrapper"
+                                className={`btn btn-sm btn-primary btn-file`}
+                                style={{ marginTop: "8px", marginRight: "8px" }}
+                            >
+                                {input.encoded_image ? "Replace Image" : "Upload Image"}
+                                <input
+                                    id="loadimage"
+                                    type="file"
+                                    style={{ display: "none" }}
+                                    onChange={e => onImageSelected(e)}
+                                />
+                            </label>
+                            <button type="button" className="btn btn-sm btn-primary" onClick={onEditImage}>Edit Image</button>
+
+                        </div>
+                        {/* <div className="form-group">
                                 <button type="button" className="btn btn-sm btn-primary" onClick={onRandomizeImage}>Randomize Image</button>
                             </div> */}
 
-                            <div className="form-group">
-                                {/* Cancel button "/" */}
-                                <button onClick={onCancel} type="button" className="btn btn-secondary">Cancel</button>
-                                &nbsp;
-                                <button type="submit" className="btn btn-primary">Create</button>
-                            </div>
+                        <div className="form-group">
+                            {/* Cancel button "/" */}
+                            <button onClick={onCancel} type="button" className="btn btn-secondary">Cancel</button>
+                            &nbsp;
+                            <button type="submit" className="btn btn-primary">Create</button>
+                        </div>
 
-                        </form>
-                    </div>
+                    </form>
                 </div>
-                {editingImage && (
-                    <ImageEditor
-                        encodedImage={`${editingImage}`}
-                        onCancel={() => setEditingImage(null)}
-                        onSave={onImageEdited}
-                    />
-                )}
-            </>
-            )
+            </div>
+            {editingImage && (
+                <ImageEditor
+                    encodedImage={`${editingImage}`}
+                    onCancel={() => setEditingImage(null)}
+                    onSave={onImageEdited}
+                />
+            )}
+        </>
+    )
 
 
 }
