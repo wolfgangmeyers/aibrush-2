d6ac55668a65f11d3c23b14a0b33a1a7e6cf9e20
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-01-17 22:17:40 -0800

    Fix broken migration

--- backend/src/migrations/007-suggestions-drop-length.sql
+++ backend/src/migrations/007-suggestions-drop-length.sql
@@ -1,3 +1,3 @@
 -- drop min_length and max_length columns from suggestions_jobs table
-ALTER TABLE suggestions_jobs DROP COLUMN min_length;
-ALTER TABLE suggestions_jobs DROP COLUMN max_length;
+ALTER TABLE suggestions_jobs DROP COLUMN IF EXISTS min_length;
+ALTER TABLE suggestions_jobs DROP COLUMN IF EXISTS max_length;
