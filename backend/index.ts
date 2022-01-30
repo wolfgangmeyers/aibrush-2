4500f88041823a3a9a43654d245eb65d3d9c8d26
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-01-30 15:35:20 -0800

    Configurable from PORT variable

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import process from "process";
 
 import { BackendService } from "./src/backend"
 import { loadConfig } from "./src/config";
@@ -7,7 +8,7 @@ import { Server } from "./src/server"
 const config = loadConfig();
 
 const backendService = new BackendService(config)
-const server = new Server(config, backendService, 3000)
+const server = new Server(config, backendService, parseInt(process.env.port || "3000"))
 
 server.init().then(() => {
     console.log("Server initialized")
