3a1677614c36bdbc31645b221ecfe1e5997d8ad7
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-01-30 15:38:07 -0800

    Fix log message

--- backend/index.ts
+++ backend/index.ts
@@ -8,11 +8,12 @@ import { Server } from "./src/server"
 const config = loadConfig();
 
 const backendService = new BackendService(config)
+const port =  parseInt(process.env.port || "3000")
 const server = new Server(config, backendService, parseInt(process.env.port || "3000"))
 
 server.init().then(() => {
     console.log("Server initialized")
     server.start().then(() => {
-        console.log("Server started on port 3000")
+        console.log(`Server started on port ${port}`)
     })
 })
