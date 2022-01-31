687cc301676a1ab655bfe6b14f4e1fec7154bf4a
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-01-30 18:59:49 -0800

    Fix asset url for downloading videos

--- frontend/src/components/ImageThumbnail.tsx
+++ frontend/src/components/ImageThumbnail.tsx
@@ -59,7 +59,7 @@ export const ImageThumbnail: FC<ImageThumbnailProps> = ({ assetsUrl, image, onCl
                 {onDesign && <button className="btn btn-secondary btn-sm" onClick={() => onDesign && onDesign(image)} style={{marginRight: "5px"}}>
                     <i className="fas fa-pencil-alt"></i>
                 </button>}
-                {(image.status === "completed" || image.status === "saved") && image.enable_video && <button className="btn btn-secondary btn-sm" onClick={() => window.open(`${assetsUrl}/${image.id}.video.mp4`)} style={{marginRight: "5px", marginTop: "5px"}}>
+                {(image.status === "completed" || image.status === "saved") && image.enable_video && <button className="btn btn-secondary btn-sm" onClick={() => window.open(`${assetsUrl}/${image.id}.mp4`)} style={{marginRight: "5px", marginTop: "5px"}}>
                     <i className="fas fa-video"></i>
                 </button>}
             </div>
