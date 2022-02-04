84a07f601d6530df8d8a7d71561ba617e31329da
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-02-03 18:06:51 -0800

    Pausing for more tests...

---
+++ frontend/src/pages/WorkerConfig.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+
+import { AIBrushApi } from "../client/api";
+
+interface Props {
+    api: AIBrushApi;
+}
+
+export const WorkerConfigPage: FC<Props> = (props) => {
+    // TODO: show some information about what a worker is and does
+    // TODO: link to github setup instructions
+    // TODO: public / private selector
+    // TODO: Download button
+}
