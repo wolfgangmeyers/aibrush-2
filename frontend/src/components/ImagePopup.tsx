d0ddf3a600dfc92128c2760b27729e2ae8dbccd6
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-02-14 12:06:23 -0800

    Fix video download on image popup

--- frontend/src/components/ImagePopup.tsx
+++ frontend/src/components/ImagePopup.tsx
@@ -70,7 +70,7 @@ export const ImagePopup: FC<ImagePopupProps> = ({ apiUrl, assetsUrl, image, onCl
                         {onDesign && <button className="btn btn-secondary btn-sm" onClick={() => onDesign && onDesign(image)} style={{ marginRight: "5px" }}>
                             <i className="fas fa-pencil-alt"></i>
                         </button>}
-                        {(image.status === "completed" || image.status === "saved") && image.enable_video && <button className="btn btn-secondary btn-sm" onClick={() => window.open(`${apiUrl}/images/${image.id}/video.mp4`)} style={{ marginRight: "5px", marginTop: "5px" }}>
+                        {(image.status === "completed" || image.status === "saved") && image.enable_video && <button className="btn btn-secondary btn-sm" onClick={() => window.open(`${apiUrl}/api/images/${image.id}.mp4`)} style={{ marginRight: "5px", marginTop: "5px" }}>
                             <i className="fas fa-video"></i>
                         </button>}
                     </div>
