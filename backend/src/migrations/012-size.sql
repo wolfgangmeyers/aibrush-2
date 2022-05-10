1ab98f2a521651b82a2fbce3bf59f3f577bebcb5
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-05-09 17:00:20 -0700

    Fix broken migrations

--- backend/src/migrations/012-size.sql
+++ backend/src/migrations/012-size.sql
@@ -1,2 +1,2 @@
 -- Add size (int) column to table "images"
-ALTER TABLE images ADD COLUMN size int NOT NULL DEFAULT 512 IF NOT EXISTS;
+ALTER TABLE images ADD COLUMN IF NOT EXISTS size int NOT NULL DEFAULT 512;
