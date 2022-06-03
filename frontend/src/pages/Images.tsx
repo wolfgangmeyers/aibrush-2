dd1c34ad5dfcb1c6bd45ec5afccd3a2ebc4349fc
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-06-03 10:04:59 -0700

    Even better sorting

--- frontend/src/pages/Images.tsx
+++ frontend/src/pages/Images.tsx
@@ -123,7 +123,7 @@ export const ImagesPage: FC<Props> = ({ api, apiUrl, assetsUrl }) => {
     const sortImages = (a: Image, b: Image) => {
         // if the parent is the same, sort by score descending
         // otherwise, sort by updated_at
-        if (a.parent && a.parent === b.parent) {
+        if (a.parent === b.parent && a.phrases.join("|") == b.phrases.join("|")) {
             // if the score is the same, sort by updated_at
             if (a.score === b.score) {
                 return b.updated_at - a.updated_at
