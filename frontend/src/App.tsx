13783e0bc9d265162f036ce67185c8d3d6eee7d4
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-09-03 12:53:34 -0700

    Login page almost matches new design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import * as axios from "axios";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 import "./bootstrap.min.css";
 import { AIBrushApi, LoginResult, FeatureList } from "./client/api";
-import { getConfig } from './config';
-import { Login } from "./pages/Login"
-import { MainMenu } from './pages/MainMenu';
-import { CreateImage } from "./pages/CreateImage"
+import { getConfig } from "./config";
+import { Login } from "./pages/Login";
+import { MainMenu } from "./pages/MainMenu";
+import { CreateImage } from "./pages/CreateImage";
 import { ImagesPage } from "./pages/Images";
 import { TokenRefresher } from "./components/TokenRefresher";
-import { Healthchecker } from './components/Healthchecker';
+import { Healthchecker } from "./components/Healthchecker";
 import { SuggestionsPage } from "./pages/Suggestions";
 import { WorkerConfigPage } from "./pages/WorkerConfig";
 import { Admin } from "./pages/Admin";
 import { CreateWorkflow } from "./pages/CreateWorkflow";
 import { Workflows } from "./pages/Workflows";
-import { WorkflowDetail } from "./pages/WorkflowDetail"
+import { WorkflowDetail } from "./pages/WorkflowDetail";
 
-const config = getConfig()
+const config = getConfig();
 const httpClient = axios.default;
-const client = new AIBrushApi(undefined, localStorage.getItem("apiUrl") || config.apiUrl, httpClient);
+const client = new AIBrushApi(
+  undefined,
+  localStorage.getItem("apiUrl") || config.apiUrl,
+  httpClient
+);
 
 function updateHttpClient(loginResult: LoginResult) {
   if (loginResult.accessToken) {
-    httpClient.defaults.headers.common['Authorization'] = `Bearer ${loginResult.accessToken}`;
+    httpClient.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${loginResult.accessToken}`;
   }
 }
 
 function App() {
-
   const [credentials, setCredentials] = useState<LoginResult | null>(null);
   const [assetsUrl, setAssetsUrl] = useState<string>("/api/images");
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
@@ -38,20 +43,22 @@ function App() {
   const onLogout = () => {
     setCredentials(null);
     localStorage.removeItem("credentials");
-    httpClient.defaults.headers.common['Authorization'] = undefined;
-  }
+    httpClient.defaults.headers.common["Authorization"] = undefined;
+  };
 
   const init = async () => {
-    console.log("App.init")
-    client.getAssetsUrl().then(result => setAssetsUrl(result.data.assets_url));
-    client.getFeatures().then(result => setFeatures(result.data));
+    console.log("App.init");
+    client
+      .getAssetsUrl()
+      .then((result) => setAssetsUrl(result.data.assets_url));
+    client.getFeatures().then((result) => setFeatures(result.data));
     const storedCredentials = localStorage.getItem("credentials");
     if (storedCredentials) {
       // attempt to refresh token
       try {
         const credentials = JSON.parse(storedCredentials) as LoginResult;
         const result = await client.refresh({
-          refreshToken: credentials.refreshToken
+          refreshToken: credentials.refreshToken,
         });
         setCredentials(result.data);
         // save to storage
@@ -77,39 +84,53 @@ function App() {
 
   return (
     <div className="App">
-      <TokenRefresher api={client} credentials={credentials as LoginResult} onCredentialsRefreshed={onLogin} />
+      <TokenRefresher
+        api={client}
+        credentials={credentials as LoginResult}
+        onCredentialsRefreshed={onLogin}
+      />
       <Healthchecker api={client} />
 
       <BrowserRouter>
-        <div className="container">
+        {/* if credentials are not set, show Login component */}
+        {!credentials && (
+          <Login httpClient={httpClient} client={client} onLogin={onLogin} />
+        )}
+        {credentials && <div className="container">
           <div className="row">
             <div className="col-lg-12">
-              {/* if credentials are not set, show Login component */}
-              {!credentials && <Login httpClient={httpClient} client={client} onLogin={onLogin} />}
               {/* if credentials are set, show a bootstrap logout button a the far top right corner div */}
-              {credentials && <>
-                <button className="btn btn-primary top-button" onClick={() => onLogout()}>
-                  {/* font awesome logout icon */}
-                  <i className="fas fa-sign-out-alt"></i>&nbsp;
-                  Logout
-                </button>
-                {/* home button */}
-                <Link className="btn btn-primary top-button" to="/">
-                  {/* font awesome home icon */}
-                  <i className="fas fa-home"></i>&nbsp;
-                  Home
-                </Link>
-                {/* Link to github project at https://github.com/wolfgangmeyers/aibrush-2 */}
-                <a className="btn btn-primary top-button" href="https://github.com/wolfgangmeyers/aibrush-2" target="_blank">
-                  {/* font awesome github icon */}
-                  <i className="fab fa-github"></i>&nbsp;
-                </a>
-              </>}
+              {credentials && (
+                <>
+                  <button
+                    className="btn btn-primary top-button"
+                    onClick={() => onLogout()}
+                  >
+                    {/* font awesome logout icon */}
+                    <i className="fas fa-sign-out-alt"></i>&nbsp; Logout
+                  </button>
+                  {/* home button */}
+                  <Link className="btn btn-primary top-button" to="/">
+                    {/* font awesome home icon */}
+                    <i className="fas fa-home"></i>&nbsp; Home
+                  </Link>
+                  {/* Link to github project at https://github.com/wolfgangmeyers/aibrush-2 */}
+                  <a
+                    className="btn btn-primary top-button"
+                    href="https://github.com/wolfgangmeyers/aibrush-2"
+                    target="_blank"
+                  >
+                    {/* font awesome github icon */}
+                    <i className="fab fa-github"></i>&nbsp;
+                  </a>
+                </>
+              )}
             </div>
           </div>
 
           {/* if credentials are set, show the rest of the app */}
-          {credentials && <Switch>
+          
+          <Switch>
             <Route path="/" exact={true}>
               <MainMenu isAdmin={isAdmin} />
             </Route>
@@ -117,7 +138,11 @@ function App() {
               <CreateImage api={client} apiUrl={config.apiUrl} />
             </Route>
             <Route path="/images">
-              <ImagesPage apiUrl={config.apiUrl} api={client} assetsUrl={assetsUrl} />
+              <ImagesPage
+                apiUrl={config.apiUrl}
+                api={client}
+                assetsUrl={assetsUrl}
+              />
             </Route>
             {/* /suggestions route */}
             <Route path="/suggestions">
@@ -128,36 +153,50 @@ function App() {
             </Route>
 
             <Route path="/create-workflow">
-              <CreateWorkflow api={client} apiUrl={config.apiUrl} assetsUrl={assetsUrl} />
+              <CreateWorkflow
+                api={client}
+                apiUrl={config.apiUrl}
+                assetsUrl={assetsUrl}
+              />
             </Route>
             <Route path="/workflows" exact={true}>
               <Workflows api={client} />
             </Route>
             <Route path="/workflows/:id">
-              <WorkflowDetail api={client} apiUrl={config.apiUrl} assetsUrl={assetsUrl} />
+              <WorkflowDetail
+                api={client}
+                apiUrl={config.apiUrl}
+                assetsUrl={assetsUrl}
+              />
             </Route>
-            {
-              isAdmin &&
+            {isAdmin && (
               <>
                 <Route path="/admin">
                   <Admin api={client} />
                 </Route>
-
               </>
-            }
-          </Switch>}
+            )}
+          </Switch>
           <div className="row" style={{ marginTop: "100px", padding: "50px" }}>
             <div className="col-lg-12">
               {/* show external popout pages to terms and privacy policy, if they are present in the features */}
-              {features && features.privacy_uri && <a href={features.privacy_uri} target="_blank">
-                Privacy Policy
-              </a>}
-              {features && features.terms_uri && <a href={features.terms_uri} target="_blank" style={{ marginLeft: "20px" }}>
-                Terms of Service
-              </a>}
+              {features && features.privacy_uri && (
+                <a href={features.privacy_uri} target="_blank">
+                  Privacy Policy
+                </a>
+              )}
+              {features && features.terms_uri && (
+                <a
+                  href={features.terms_uri}
+                  target="_blank"
+                  style={{ marginLeft: "20px" }}
+                >
+                  Terms of Service
+                </a>
+              )}
             </div>
           </div>
-        </div>
+        </div>}
       </BrowserRouter>
     </div>
   );
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,110 +1,157 @@
 // Login react component with hooks
-import React, { useState, FC } from 'react';
+import React, { useState, FC } from "react";
 import * as axios from "axios";
 import qs from "qs";
 import { AIBrushApi, LoginResult } from "../client/api";
 
 interface LoginProps {
-    httpClient: axios.AxiosInstance;
-    client: AIBrushApi;
-    onLogin: (loginResult: LoginResult) => void;
+  httpClient: axios.AxiosInstance;
+  client: AIBrushApi;
+  onLogin: (loginResult: LoginResult) => void;
 }
 
-export const Login: FC<LoginProps> = props => {
-    // login form accepts email only
-    const [email, setEmail] = useState("");
-    const [emailSubmitted, setEmailSubmitted] = useState(false);
-    const [code, setCode] = useState("");
-    const [err, setErr] = useState("");
+export const Login: FC<LoginProps> = (props) => {
+  // login form accepts email only
+  const [email, setEmail] = useState("");
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [code, setCode] = useState("");
+  const [err, setErr] = useState("");
 
-    // check query string for invite_code
-    const query = window.location.search;
-    const queryParams = qs.parse(query.substring(1));
-    const inviteCode = queryParams["invite_code"];
+  // check query string for invite_code
+  const query = window.location.search;
+  const queryParams = qs.parse(query.substring(1));
+  const inviteCode = queryParams["invite_code"];
 
-    const onLogin = async () => {
-        // clear error
-        setErr("");
-        // validate valid email
-        if (!email.match(/^[^@]+@[^@]+\.[^@]+$/)) {
-            setErr("Invalid email address");
-            return;
-        }
-        
-        try {
-            await props.client.login({
-                email: email,
-                invite_code: inviteCode as string,
-            })
-            setEmailSubmitted(true);
-        } catch (err) {
-            console.error(err);
-            setErr("Could not login");
-        }
+  const onLogin = async () => {
+    // clear error
+    setErr("");
+    // validate valid email
+    if (!email.match(/^[^@]+@[^@]+\.[^@]+$/)) {
+      setErr("Invalid email address");
+      return;
     }
 
-    const onVerify = async () => {
-        // clear error
-        setErr("");
-        // attempt to verify code
-        try {
-            const result = await props.client.verify({
-                email,
-                code
-            });
-            props.onLogin(result.data);
-        } catch (err) {
-            console.error(err)
-            setErr("Failed to verify code");
-        }
+    try {
+      await props.client.login({
+        email: email,
+        invite_code: inviteCode as string,
+      });
+      setEmailSubmitted(true);
+    } catch (err) {
+      console.error(err);
+      setErr("Could not login");
     }
+  };
 
-    // bootstrap login form
-    return (
-        <>
-            <div className="row">
-                <div className="col-md-6 offset-md-3" style={{marginTop: "50px"}}>
-                    <p className="text-info">
-                        Log in below using your email. After submitting, you will be prompted for a login code. Check your email inbox for the code and enter it to log in.
-                    </p>
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Login</h5>
-                            {/* Display error if one is set */}
-                            {/* Use <p class="text-danger" */}
-                            {err && <p className="text-danger">{err}</p>}
-                            {err && <p className="text-info">If you don't have an account yet, request one by sending an email to <a href="mailto:admin@aibrush.art">admin@aibrush.art</a></p>}
-                            {/* If email submitted, show verify code form*/}
-                            {emailSubmitted && <div>
-                                    <div className="form-group">
-                                        <label htmlFor="verifyCode">Verify Code</label>
-                                        <input type="text" className="form-control" id="verifyCode" placeholder="Verification Code" value={code} onChange={(e) => setCode(e.target.value)} />
-                                    </div>
-                                    {/* Cancel verification and go back to email */}
-                                    <button className="btn btn-secondary" onClick={() => setEmailSubmitted(false)}>Cancel</button>&nbsp;
-                                    {/* Verify code */}
-                                    <button type="button" className="btn btn-primary" onClick={(e) => {
-                                        e.preventDefault();
-                                        onVerify();
-                                    }}>Verify</button>
-                                </div>
-                            }
-                            {/* Else, show login form */}
-                            {!emailSubmitted && <div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email address</label>
-                                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                                </div>
-                                <button type="button" className="btn btn-primary" onClick={(e) => {
-                                    e.preventDefault();
-                                    onLogin();
-                                }}>Login</button>
-                            </div>}
-                        </div>
-                    </div>
+  const onVerify = async () => {
+    // clear error
+    setErr("");
+    // attempt to verify code
+    try {
+      const result = await props.client.verify({
+        email,
+        code,
+      });
+      props.onLogin(result.data);
+    } catch (err) {
+      console.error(err);
+      setErr("Failed to verify code");
+    }
+  };
+
+  // bootstrap login form
+  return (
+    <>
+      <div className="row" style={{height: "100vh", marginLeft: "0px", marginRight: "0px"}}>
+        <div className="col-md-6 login-splash">
+          {/* <div className="center-cropped" style={{backgroundImage: "url(/images/scifi-dreamland.jpg)"}}></div> */}
+          <img
+            className="center-cropped"
+            src="/images/scifi-dreamland.jpg"
+          />
+        </div>
+        <div className="col-md-6">
+          <div
+            style={{ fontWeight: 500, fontSize: "48px", marginTop: "241px" }}
+          >
+            AiBrush
+          </div>
+          {/* Display error if one is set */}
+          {/* Use <p class="text-danger" */}
+          {err && <p className="text-danger">{err}</p>}
+          {err && (
+            <p className="text-info">
+              If you don't have an account yet, request one by sending an email
+              to <a href="mailto:admin@aibrush.art">admin@aibrush.art</a>
+            </p>
+          )}
+          <div style={{marginLeft: "10%", marginRight: "10%", marginTop: "88px", textAlign: "left"}}>
+            {/* If email submitted, show verify code form*/}
+            {emailSubmitted && (
+              <>
+                <div className="form-group">
+                  <label htmlFor="verifyCode">Verify Code</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="verifyCode"
+                    placeholder="Verification Code"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                  />
                 </div>
-            </div>
-        </>
-    );
-}
+                {/* Cancel verification and go back to email */}
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => setEmailSubmitted(false)}
+                >
+                  Cancel
+                </button>
+                &nbsp;
+                {/* Verify code */}
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    onVerify();
+                  }}
+                >
+                  Verify
+                </button>
+              </>
+            )}
+            {/* Else, show login form */}
+            {!emailSubmitted && (
+              <>
+                <div className="form-group">
+                  <label htmlFor="email">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  style={{float: "right"}}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    onLogin();
+                  }}
+                >
+                  Login
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
