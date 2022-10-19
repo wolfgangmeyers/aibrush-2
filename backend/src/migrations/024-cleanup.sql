3b277072aa580b9cd180a1f6494028eff4e55d95
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-10-18 18:35:56 -0700

    Drop unused tables

---
+++ backend/src/migrations/024-cleanup.sql
@@ -0,0 +1,6 @@
+-- cleanup resources for retired features
+DROP TABLE IF EXISTS suggestions_jobs;
+DROP TABLE IF EXISTS suggestion_seeds;
+DROP TABLE IF EXISTS svg_jobs;
+DROP TABLE IF EXISTS workflow_events;
+DROP TABLE IF EXISTS workflows;
