a0ae46697f4a4b0fe9d7c95c24118dabb5421617
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2022-11-27 17:33:41 -0800

    Wipe out existing worker configs

--- backend/src/migrations/032-update-worker-configs.sql
+++ backend/src/migrations/032-update-worker-configs.sql
@@ -1,9 +1,4 @@
--- CREATE TABLE IF NOT EXISTS worker_configs (
---     worker_id CHAR(36) PRIMARY KEY,
---     model text NOT NULL,
---     pool_assignment text NOT NULL,
---     FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE
--- );
+DELETE FROM worker_configs;
 
 -- drop model and pool_assignment columns, add config_json column
 ALTER TABLE worker_configs
