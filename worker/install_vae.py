6497f0c2109fe8f94f011647f0c10c23530e1e5c
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2023-06-18 23:52:31 -0700

    Added safetensors support to vae script

--- worker/install_vae.py
+++ worker/install_vae.py
@@ -1,6 +1,7 @@
 import sys
 import torch
 import os
+from safetensors.torch import load_file, save_file
 
 sd_file = os.path.expanduser(sys.argv[1])
 # vae_file = sys.argv[2]
@@ -8,11 +9,20 @@ sd_file = os.path.expanduser(sys.argv[1])
 vae_file = os.path.expanduser(sys.argv[2])
 out_file = os.path.expanduser(sys.argv[3])
 
-sd_model = torch.load(sd_file, map_location="cpu")["state_dict"]
-vae_model = torch.load(vae_file, map_location="cpu")["state_dict"]
+if sd_file.endswith(".safetensors"):
+    sd_model = load_file(sd_file, device="cpu")
+else:
+    sd_model = torch.load(sd_file, map_location="cpu")["state_dict"]
+if vae_file.endswith(".safetensors"):
+    vae_model = load_file(vae_file, device="cpu")["state_dict"]
+else:
+    vae_model = torch.load(vae_file, map_location="cpu")["state_dict"]
 
 for vae_key in vae_model:
     sd_key = f"first_stage_model.{vae_key}"
     sd_model[sd_key] = vae_model[vae_key]
 
-torch.save({"state_dict": sd_model}, out_file)
+if out_file.endswith(".safetensors"):
+    save_file(sd_model, out_file)
+else:
+    torch.save({"state_dict": sd_model}, out_file)
