171239f7c49ca1ccd146b34f04db28688d9c0b72
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2023-06-25 18:14:30 -0700

    Shorten alchemy timeout to 1m

--- horde_worker/horde.ts
+++ horde_worker/horde.ts
@@ -194,7 +194,7 @@ export async function processAlchemistImage(
                 console.log(`completed in ${moment().diff(start, "seconds")} seconds`)
                 return webpImageResponse.data;
             } else {
-                if (moment().diff(start, "seconds") > 200) {
+                if (moment().diff(start, "seconds") > 60) {
                     console.log("Horde request timed out");
                     await axios.delete(`${hordeBaseUrl}/v2/interrogate/status/${reqId}`, {
                         headers: {
