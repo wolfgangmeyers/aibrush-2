061856079932ae0c37a73d3d179e5da1fe7a2dcd
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2023-07-22 16:54:28 -0700

    Remove worker folder

--- worker/memutil.py
+++
@@ -1,9 +0,0 @@
-from typing import Tuple
-import torch
-
-def get_free_memory() -> Tuple[int, int]:
-    free, total = torch.cuda.mem_get_info()
-    print(f"Free memory: {free}")
-    print(f"Total memory: {total}")
-
-    return free
